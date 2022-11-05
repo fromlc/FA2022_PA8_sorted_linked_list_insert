36ad4845042892b8eff995a4245867337e2668d5
L Carver <wifihum@gmail.com>
2022-11-05 14:23:28 -0700

    upgrade driver

--- source/app.cpp
+++ source/app.cpp
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// app.cpp : LinkedList class demo
+// app.cpp : LinkedList class driver
 //------------------------------------------------------------------------------
 #include <iostream>
 
@@ -11,8 +11,24 @@
 using std::cout;
 
 //------------------------------------------------------------------------------
+// constants
+//------------------------------------------------------------------------------
+constexpr bool INS = true;
+constexpr bool ADD = !INS;
+
+//------------------------------------------------------------------------------
 // local function prototypes
 //------------------------------------------------------------------------------
+void testAdd(LinkedList &myList);
+void testInsert(LinkedList& myList);
+void testDelete(LinkedList& myList);
+
+// delete node, show remaining list data
+inline bool _zapNshow(LinkedList& list, int data);
+
+// insert (true) or add (false) new node and show list data
+inline void _newNshow(LinkedList& list, int data, bool insert = INS);
+
 void displayNodeData(LinkedList& dataList);
 
 //------------------------------------------------------------------------------
@@ -21,41 +37,124 @@ void displayNodeData(LinkedList& dataList);
 int main() {
 	// local LinkedList class instance declaration allocates stack memory
 	LinkedList myList;
-	// for testing
-	bool deleted;
 
-	// add some list items, each one becomes the new list head
+	//--------------------------------------------------------------------------
+	// add some list items, on add each one becomes the new list head
+	//--------------------------------------------------------------------------
+	testAdd(myList);
 
-	myList.addNode(1);
-	myList.addNode(2);
-	myList.addNode(3);
-	myList.addNode(4);
-	myList.addNode(5);
+	//--------------------------------------------------------------------------
+	// insert some list items in ascending sort order
+	//--------------------------------------------------------------------------
+	testInsert(myList);
 
-	// show initial list data
-	displayNodeData(myList);
-
-	// check all four cases deleteNode() must handle
-	// for thorough list test check all four
-	// several times in different orders
-	
-	// delete node in the middle of the list
-	deleted = myList.deleteNode(3);
-	// delete node at list end
-	deleted = myList.deleteNode(1);
-	// delete node at list head
-	deleted = myList.deleteNode(5);
-	// delete node that isn't in the list
-	deleted = myList.deleteNode(100);
-
-	// show remaining list data
-	displayNodeData(myList);
-
-	// release all node memory
+	// END TEST release all node memory
 	myList.makeEmpty();
 
 	// good c++itizen
 	return 0;
+}
+
+//------------------------------------------------------------------------------
+// exercise addNode()
+//
+// add some list items, on add each one becomes the new list head
+//------------------------------------------------------------------------------
+void testAdd(LinkedList& myList) {
+
+	_newNshow(myList, 1, ADD);
+	_newNshow(myList, 2, ADD);
+	_newNshow(myList, 3, ADD);
+	_newNshow(myList, 4, ADD);
+	_newNshow(myList, 5, ADD);
+}
+
+//------------------------------------------------------------------------------
+// exercise insertNode()
+//
+// check all four cases insertNode() must handle
+// for a thorough list test check all cases
+// several times in different orders
+//
+//	case 1: new data node is first list node
+//	case 2: new data node becomes new head of list
+//	case 3: new data node added after tail, becomes new tail of list
+//	case 4: new data node inserted between nodes
+//------------------------------------------------------------------------------
+void testInsert(LinkedList& myList) {
+
+	// case 1: new data node is first list node
+	_newNshow(myList, 3, INS);
+	// case 2: new data node becomes new head of list
+	_newNshow(myList, 1, INS);
+	// case 4: new data node inserted between nodes
+	_newNshow(myList, 2, INS);
+	// case 3: new data node added after tail, becomes new tail of list
+	_newNshow(myList, 5, INS);
+	// case 4: new data node inserted between nodes
+	_newNshow(myList, 4, INS);
+	// case 3: new data node added after tail, becomes new tail of list
+	_newNshow(myList, 5, INS);
+}
+
+//------------------------------------------------------------------------------
+// exercise deleteNode()
+//
+// check all four cases deleteNode() must handle
+// for a thorough list test check all cases
+// several times in different orders
+//------------------------------------------------------------------------------
+void testDelete(LinkedList& myList) {
+
+	// for testing deleteNode() return value
+	bool deleted;
+
+	// case 1: delete node in the middle of the list
+	deleted = _zapNshow(myList, 3);
+
+	// case 2: delete node at list head
+	deleted = _zapNshow(myList, 1);
+
+	// case 3: delete node at list tail
+	deleted = _zapNshow(myList, 5);
+
+	// delete node at list tail again
+	deleted = _zapNshow(myList, 5);
+
+	// case 4: delete node that's not in the list
+	deleted = _zapNshow(myList, 100);
+}
+
+//------------------------------------------------------------------------------
+// delete node, show remaining list data
+//------------------------------------------------------------------------------
+inline bool _zapNshow(LinkedList& list, int data) {
+
+	cout << "\ndeleting " << data;
+
+	bool deleted = list.deleteNode(3);
+	displayNodeData(list);
+
+	return deleted;
+}
+
+//------------------------------------------------------------------------------
+// add or insert node then show list data
+// 
+// inserts node when insert is true, adds node otherwise
+//------------------------------------------------------------------------------
+inline void _newNshow(LinkedList& list, int data, bool insert) {
+
+	if (insert) {
+		cout << "\ninserting sorted " << data;
+		list.insertNode(data);
+	}
+	else {
+		cout << "\nadding unsorted " << data;
+		list.addNode(data);
+	}
+
+	displayNodeData(list);
 }
 
 //------------------------------------------------------------------------------
@@ -95,16 +194,17 @@ void displayNodeData(LinkedList& dataList) {
 	// reset list position pointer to list head
 	dataList.gotoHead();
 	
-	std::cout << "\nat head of list";
+	//cout << "\nat list head";
 	// walk the list, get each node data item, display it
 	do {
 		// "bucket" for receiving node data in reference parameter
 		int intDataBucket;
 		if (dataList.getCurrentNodeData(intDataBucket)) {
-			std::cout << '\n' << intDataBucket;
+			cout << '\n' << intDataBucket;
 		}
 		else {
-			std::cout << "\nat end of list\n";
+			//cout << "\nat list tail\n";
+			cout << '\n';
 		}
 	} while (dataList.gotoNext());
 	//-----------------------------------------------------------------------------
